b022062f67983df90c62064c9b21fa11ee644d60
Egor Nedelko <gojiramedia@gmail.com>
2020-09-30 15:05:27 +0300

    feat: define 'min' function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
 }
+
 
 exports.max = function max (array) {
   return 0;
